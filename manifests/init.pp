5df7226d943671383d879204850b2ef41cc8a366
root <root@client1.example.com>
2013-01-21 02:55:54 +0000

    commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,10 +2,9 @@ class local-openstack {
   
 
   Apt::Source['folsom_trunk_testing'] ->
-  Package['git'] ->
-# Apt::Source['ubuntu_cloud_archive'] -> 
   Package['apache2'] ->
-  Service['apache2'] # -> Class['openstack::all']
+  Apt::Source['ubuntu_cloud_archive'] ->
+  Service['apache2'] -> Class['openstack::all']
 
   package { 'python-software-properties':
     ensure            => 'latest',
@@ -35,26 +34,28 @@ class local-openstack {
     ensure               => 'running',
     subscribe            => Package['apache2'],
   } 
-# class { 'openstack::all':
-#   public_address       => '192.168.1.12',
-#   public_interface     => 'eth0',
-#   private_interface    => 'eth1',
-#   admin_email          => 'some_admin@some_company',
-#   admin_password       => 'admin_password',
-#   nova_db_password     => 'password',
-#   glance_db_password   => 'password',
-#   keystone_db_password => 'password',
-#   secret_key           => 'password',
-#   mysql_root_password  => 'password',
-#   quantum              => false,
-#   keystone_admin_token => 'keystone_admin_token',
-#   nova_user_password   => 'nova_user_password',
-#   glance_user_password => 'glance_user_password',
-#   rabbit_password      => 'rabbit_password',
-#   rabbit_user          => 'rabbit_user',
+  class { 'openstack::all':
+    public_address       => '10.0.2.15',
+    public_interface     => 'eth0',
+    private_interface    => 'eth1',
+    admin_email          => 'some_admin@some_company',
+    admin_password       => 'admin_password',
+    nova_db_password     => 'password',
+    glance_db_password   => 'password',
+    keystone_db_password => 'password',
+    secret_key           => 'password',
+    mysql_root_password  => 'password',
+    quantum              => false,
+    keystone_admin_token => 'keystone_admin_token',
+    nova_user_password   => 'nova_user_password',
+    glance_user_password => 'glance_user_password',
+    rabbit_password      => 'rabbit_password',
+    rabbit_user          => 'rabbit_user',
 #   libvirt_type         => 'kvm',
-#   fixed_range          => '10.0.2.0/24',
-# }
+    libvirt_type         => 'qemu',
+    fixed_range          => '172.16.32.0/24',
+  }
+
   package { 'git':
     ensure            => 'latest',
   }
