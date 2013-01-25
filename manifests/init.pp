ba293e3efd87a73ded7bb238888def9fa8c5bf57
root <root@firefly.e271.net>
2013-01-25 15:04:21 -0500

    got it working

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,73 @@
 class local-openstack {
   
 
-  Apt::Source['folsom_trunk_testing'] ->
-  Package['apache2'] ->
-  Apt::Source['ubuntu_cloud_archive'] ->
-  Service['apache2'] -> Class['openstack::all']
+# Apt::Source['folsom_trunk_testing'] ->
 
+  Package['python-software-properties'] ->
+# Package['bridge-utils'] ->
+  Package['git'] ->
+  Package['lynx'] ->
+  Package['ubuntu-cloud-keyring'] ->
+  File['/etc/apt/sources.list'] ->
+  Exec['apt-get update'] ->
+  Exec['brctl addbr br100'] ->
+  Exec['ifconfig br100 192.168.100.1'] # ->
+  File['/etc/apache2/mods-available/wsgi.conf'] ~>
+  Package['apache2'] ->
+# Apt::Source['ubuntu_cloud_archive'] ->
+  Service['apache2']   -> 
+  Class['openstack::all']
+
+  package { 'ntfs-3g':
+    ensure            => 'absent',
+  }
   package { 'python-software-properties':
     ensure            => 'latest',
   }
-  apt::source { 'ubuntu_cloud_archive':
-    location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-    release           => 'precise-updates/folsom',
-    repos             => 'main',
-    key               => 'EC4926EA',
-    key_server        => 'subkeys.pgp.net',
-    pin               => '-10',
-    include_src       => true
-  } 
+# package { 'bridge-utils':
+#   ensure            => 'latest',
+# }
+  package { 'git':
+    ensure            => 'latest',
+  }
+  package { 'lynx':
+    ensure            => 'latest',
+  }
+  package { 'ubuntu-cloud-keyring':
+    ensure            => 'latest',
+  }
+  file { '/etc/apt/sources.list':
+    ensure            => 'file',
+    source            => 'puppet:///modules/local-openstack/sources.list',
+    owner             => 'root',
+    mode              => '0644',
+  }
+  exec {'apt-get update':
+    path              => '/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin',
+    subscribe         => File['/etc/apt/sources.list'],
+    refreshonly       => true,
+  }
+  exec { 'brctl addbr br100':
+    path              => '/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin',
+    onlyif            => "ifconfig -a |grep br100| wc -l |grep 0 > /dev/null",
+    subscribe         => Package['bridge-utils'],
+  }
+  exec { 'ifconfig br100 192.168.100.1':
+    path              => '/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin',
+    onlyif            => 'ifconfig br100|grep 192.168.100.1|wc -l|grep 0 > /dev/null',
+    subscribe         => Exec['brctl addbr br100'],
+    alias             => 'ifconfig-bridge',
+  }
+
+# apt::source { 'ubuntu_cloud_archive':
+#   location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
+#   release           => 'precise-updates/folsom',
+#   repos             => 'main',
+#   key               => 'EC4926EA',
+#   key_server        => 'subkeys.pgp.net',
+#   pin               => '-10',
+#   include_src       => true
+# } 
 # apt::source { 'folsom_trunk_testing':
 #   location          => 'http://ppa.launchpad.net/openstack-ubuntu-testing/folsom-trunk-testing/ubuntu',
 #   release           => 'precise',
@@ -37,7 +87,7 @@ class local-openstack {
   class { 'openstack::all':
     public_address       => '10.0.2.15',
     public_interface     => 'eth0',
-    private_interface    => 'eth1',
+    private_interface    => 'br100',
     admin_email          => 'some_admin@some_company',
     admin_password       => 'admin_password',
     nova_db_password     => 'password',
@@ -53,11 +103,13 @@ class local-openstack {
     rabbit_user          => 'rabbit_user',
 #   libvirt_type         => 'kvm',
     libvirt_type         => 'qemu',
-    fixed_range          => '172.16.32.0/24',
+    fixed_range          => '192.168.100.0/24',
   }
-
-  package { 'git':
-    ensure            => 'latest',
+  file { '/etc/apache2/mods-available/wsgi.conf':
+    ensure               => 'file',
+    owner                => 'root',
+    mode                 => '0644',
+    source               => 'puppet:///modules/local-openstack/wsgi.conf',
   }
 }
 
